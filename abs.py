98ef6feda6cef9ad1d789758af02e67c15e9a590
Divyansh <divyansh.kumar222@gmail.com>
2019-06-15 08:19:54 +0530

    Create abs.py

---
+++ abs.py
@@ -0,0 +1,6 @@
+''' Find the absolute value of a number entered through the
+keyboard. '''
+
+import math 
+n = float(input( " Enter any number "))
+print(" The absolute value of " + str(n) + " is " + str( math.fabs(n)))
