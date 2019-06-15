8ecde12eaf98998560b3c396b7ffeee85684c76a
Divyansh <divyansh.kumar222@gmail.com>
2019-06-15 08:21:31 +0530

    Create sum10.py

---
+++ sum10.py
@@ -0,0 +1,15 @@
+# Input Ten Numbers and calculate its sum.
+
+n1 = int(input(" Enter 1st number "))
+n2 = int(input(" Enter 2nd number "))
+n3 = int(input(" Enter 3rd number "))
+n4 = int(input(" Enter 4th number "))
+n5 = int(input(" Enter 5th number "))
+n6 = int(input(" Enter 6th number "))
+n7 = int(input(" Enter 7th number "))
+n8 = int(input(" Enter 8th number "))
+n9 = int(input(" Enter 9th number "))
+n10 =int(input(" Enter 10th number "))
+ans = n1+n2+n3+n4+n5+n6+n7+n8+n9+n10
+print(" The result of Addition of 10 numbers is : " + str(ans))
+# print(" The result of Addition of 10 numbers is : " , ans)
