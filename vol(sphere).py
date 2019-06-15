e532be4c7418c827863753db843983d385121c1b
Divyansh <divyansh.kumar222@gmail.com>
2019-06-15 08:21:46 +0530

    Create vol(sphere).py

---
+++ vol(sphere).py
@@ -0,0 +1,5 @@
+# Write a C program that calculates the volume of a sphere.
+
+radius = int(input ("Enter the radius ") )
+volume = (4/3*3.14)*(radius**3)
+print(" The volume of sphere is " + str(volume))
