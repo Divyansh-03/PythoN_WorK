1343cb3d318981c92589a721c545027d57e2fbc1
Divyansh <divyansh.kumar222@gmail.com>
2019-06-15 08:20:38 +0530

    Create FtoD.py

---
+++ FtoD.py
@@ -0,0 +1,6 @@
+''' Temperature of a city in Fahrenheit degrees is input through
+the keyboard. Write a program to convert this temperature
+into Centigrade degrees.'''
+
+temp_f= float(input(" Enter Temperature in Fahrenheit "))
+print (' The temperature in Celcius is ' + str(((temp_f -32)*5)/9))
