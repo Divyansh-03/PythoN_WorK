7f00157dad4de5f39cdb682c1564dafb84de75ae
Divyansh <divyansh.kumar222@gmail.com>
2019-06-18 08:37:03 +0530

    Create 5eveniii.py

---
+++ 5eveniii.py
@@ -0,0 +1,13 @@
+''' Input Any Number and Display First that numberth Even Number Series
+For e,g,
+7
+
+2    4    6    8    10     12    14  '''
+
+
+i=int(input(" Enter any number to display that numberth even series "))
+j=2
+while i:
+	print(str(j) + "\n")
+	j=j+2
+	i=i-1
