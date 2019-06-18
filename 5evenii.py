1725b9dd50d696e37131586c9b950e00f9103997
Divyansh <divyansh.kumar222@gmail.com>
2019-06-18 08:37:13 +0530

    Create 5evenii.py

---
+++ 5evenii.py
@@ -0,0 +1,14 @@
+''' Question 2 :-
+
+Input Any Number ( range)  and Display Even Number SEries till that number
+For e.g.
+Input :- 10
+
+Output :- 
+2  4  6  8  10   '''
+
+num = int(input( " Enter any number "))
+i=2
+while i<=num:
+	print(str(i) + "\n")
+	i+=2
